0fff2eba2e75288ed6983d5f8b42976e5b5a3daa
Oren <orengolan@gmail.com>
2016-06-20 18:23:06 -0700

    misc

--- jws/main.go
+++ jws/main.go
@@ -1,25 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
 import "github.com/SermoDigital/jose/jws"
+
+const (
+	privKeyPath = "keys/app.rsa"     // openssl genrsa -out keys/app.rsa 1024
+	pubKeyPath  = "keys/app.rsa.pub" // openssl rsa -in keys/app.rsa -pubout > keys/app.rsa.pub
+)
+
+var signKey []byte
+
+func init() {
+	var err error
+	signKey, err = ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		log.Fatal("Error reading private key")
+		os.Exit(1)
+	}
+
+}
 
 func main() {
 	token := createJWT()
-	valid := validateJWT(token)
 	fmt.Println("Created token", token)
-	fmt.Println("Valid?", valid)
 }
 
 func createJWT() string {
-	var claims jws.Claims
+	claims := jws.Claims{}
 	// claims.Set("AccessToken", "level1")
-
-	signMethod := jws.GetSigningMethod("RS256")
+	signMethod := jws.GetSigningMethod("HS512")
 	token := jws.NewJWT(claims, signMethod)
-	fmt.Println("token", token)
-	return "abcd"
-}
+	byteToken, err := token.Serialize(signKey)
+	if err != nil {
+		log.Fatal("Error signing the key. ", err)
+		os.Exit(1)
+	}
 
-func validateJWT(token string) bool {
-	return true
+	return string(byteToken)
 }
